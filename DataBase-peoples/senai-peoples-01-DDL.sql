0532c6d565cfb23689cafa37104def59f62552df
Giovanni Nespoli <ggnespoli@gmail.com>
2021-04-09 18:13:09 -0300

    Projeto_Peoples.Web.AṔI

---
+++ DataBase-peoples/senai-peoples-01-DDL.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE T_Peoples;
+GO
+
+USE T_Peoples;
+GO
+
+CREATE TABLE Funcionarios(
+
+	Nome			VARCHAR(200) NOT NULL,
+	Sobrenome		VARCHAR(200) NOT NULL,
+		
+);
---
+++ Senai.Peoples.WebApi/Senai.Peoples.WebApi/Interfaces/IFuncionariosRepository.cs
@@ -0,0 +1,19 @@
+﻿using Senai.Peoples.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Peoples.WebApi.Interfaces
+{
+    public interface IFuncionariosRepository
+    {
+        List<FuncionariosDomain> ListarTodos();
+        void Cadastrar(FuncionariosDomain Funcionario);
+        FuncionariosDomain BuscarPorId(int id);
+        void Deletar(int id);
+        void AtualizarIdUrl(int id, FuncionariosDomain Funcionario);
+        void AtualizarIdCorpo(FuncionariosDomain Funcionario);
+        
+    }
+}
